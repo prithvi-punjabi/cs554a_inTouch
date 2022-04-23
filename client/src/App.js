e8317ce95fc057d2226815cdb033d0804ff5d789
Prithvi Punjabi <prithvidais@gmail.com>
2022-04-22 21:09:04 -0400

    modified resolvers to handle JWT token current user

--- client/src/App.js
+++ client/src/App.js
@@ -8,24 +8,24 @@ import Channel from "./components/Channel";
 import { AppContext, socket } from "./context/appContext";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-	return (
-		<AppContext.Provider value={{ socket: socket }}>
-			<Router>
-				<div className="App">
-					<div className="App-body">
-						<Routes>
-							<Route path="/" element={<Home />} />
-							<Route path="/token-how-to" element={<HowTo />} />
-							<Route path="/signup" element={<Signup />} />
-							<Route path="/login" element={<Login />} />
-							<Route path="/feed" element={<Feed />} />
-							<Route path="/channels" element={<Channel />} />
-						</Routes>
-					</div>
-				</div>
-			</Router>
-		</AppContext.Provider>
-	);
+  return (
+    <AppContext.Provider value={{ socket: socket }}>
+      <Router>
+        <div className="App">
+          <div className="App-body">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/token-how-to" element={<HowTo />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/feed" element={<Feed />} />
+              <Route path="/channels" element={<Channel />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
