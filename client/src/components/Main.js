ae5b9f1748085102b8e492759929cd011c1852c1
Samarth Kapuria <zeroand1@users.noreply.github.com>
2022-04-26 23:40:01 -0400

    Main Body Display

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,25 +1,44 @@
-import React from 'react'
-import styled from "styled-components"
-import Navbar from './Navbar'
-import Sidebar from './Sidebar'
-
+import { printIntrospectionSchema } from "graphql";
+import React, { useState } from "react";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Posts from "./Posts";
+//currentBody fun
 function Main() {
-  return (
-    <>
-        <Navbar></Navbar>
-        <Appbody>
-
-        <Sidebar></Sidebar>
-        
-        </Appbody>
-        
-    </>
-  )
+	const [currentBody, setCurrentBody] = useState();
+	if (currentBody === "feed") {
+		console.log("shown Feed");
+		return (
+			<>
+				<Navbar></Navbar>
+				<Appbody>
+					<Sidebar currentBody={setCurrentBody}></Sidebar>
+					<Posts></Posts>
+				</Appbody>
+				{/* <Displaybody></Displaybody> */}
+			</>
+		);
+	} else {
+		return (
+			<>
+				<Navbar></Navbar>
+				<Appbody>
+					<Sidebar currentBody={setCurrentBody}></Sidebar>
+				</Appbody>
+			</>
+		);
+	}
 }
 
-export default Main
+export default Main;
 
 const Appbody = styled.div`
-display: flex;
-height: 100vh;
-`
+	display: flex;
+	height: 100vh;
+`;
+const Displaybody = styled.div`
+	display: flex;
+	flex: 0.4;
+	height: 100vh;
+`;
