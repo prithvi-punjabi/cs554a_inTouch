b30a7650da8caf9b5400065732e61995913aff90
Prithvi Punjabi <prithvidais@gmail.com>
2022-04-30 23:18:02 -0400

    Fixed conflict

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -10,11 +10,8 @@ import Dropdown from "react-bootstrap/Dropdown";
 import AddPost from "./AddPost";
 import DeletePost from "./DeletePost";
 
-<<<<<<< HEAD
-=======
 import styled from "styled-components";
 
->>>>>>> 79a616037867ebaadba275c3cd826dbf30349f7d
 import "../App.css";
 const Posts = () => {
   const navigate = useNavigate();
@@ -28,165 +25,172 @@ const Posts = () => {
 
     return (
       <PostDiv>
-      <div className="displayContainer">
-        <AddPost userId={userId} />
-        {posts.map((post) => {
-          return (
-            <div className="container mt-5 mb-5" key={post._id}>
-              <div className="row d-flex align-items-center justify-content-center">
-                <div className="col-md-6">
-                  <div className="card">
-                    <div className="d-flex justify-content-between p-2 px-3">
-                      <div className="d-flex flex-row align-items-center">
-                        {" "}
-                        <img
-                          src={post.user.profilePicture}
-                          width="50"
-                          className="rounded-circle"
-                          alt={post.user.userName}
-                          onClick={() => navigate(`/user/${post.user._id}`)}
-                          style={{ cursor: "pointer" }}
-                        />
-                        <div className="d-flex flex-column ml-2">
+        <div className="displayContainer">
+          <AddPost userId={userId} />
+          {posts.map((post) => {
+            return (
+              <div className="container mt-5 mb-5" key={post._id}>
+                <div className="row d-flex align-items-center justify-content-center">
+                  <div className="col-md-6">
+                    <div className="card">
+                      <div className="d-flex justify-content-between p-2 px-3">
+                        <div className="d-flex flex-row align-items-center">
                           {" "}
-                          <span
-                            className="font-weight-bold"
+                          <img
+                            src={post.user.profilePicture}
+                            width="50"
+                            className="rounded-circle"
+                            alt={post.user.userName}
                             onClick={() => navigate(`/user/${post.user._id}`)}
                             style={{ cursor: "pointer" }}
-                          >
-                            {post.user.name}
-                          </span>{" "}
-                          <small className="text-primary">
-                            {post.category}
-                          </small>{" "}
-                        </div>
-                      </div>
-                      <div className="d-flex flex-row mt-1 ellipsis">
-                        {" "}
-                        <small className="mr-2">
-                          {Math.floor(
-                            (new Date() - new Date(post.dateCreated)) /
-                              (1000 * 3600 * 24)
-                          )}{" "}
-                          days ago
-                        </small>{" "}
-                        {userId === post.user._id && (
-                          <div>
-                            {/* <i className="fa fa-ellipsis-h"></i> */}
-                            <DropdownButton
-                              id="dropdown-basic-button"
-                              variant="default"
-                              size="sm"
-                              title={
-                                <span>
-                                  <i className="fa fa-ellipsis-h"></i>
-                                </span>
-                              }
+                          />
+                          <div className="d-flex flex-column ml-2">
+                            {" "}
+                            <span
+                              className="font-weight-bold"
+                              onClick={() => navigate(`/user/${post.user._id}`)}
+                              style={{ cursor: "pointer" }}
                             >
-                              <Dropdown.Item>Update</Dropdown.Item>
-                              <DeletePost postId={post._id} />
-                            </DropdownButton>
+                              {post.user.name}
+                            </span>{" "}
+                            <small className="text-primary">
+                              {post.category}
+                            </small>{" "}
                           </div>
-                        )}
-                      </div>
-                    </div>{" "}
-                    {post.image && (
-                      <img
-                        src={post.image}
-                        className="img-fluid"
-                        alt={post.text}
-                      ></img>
-                    )}
-                    <div className="p-2">
-                      <p className="text-justify">{post.text}</p>
-                      <hr />
-                      <div className="d-flex justify-content-between align-items-center">
-                        <div className="d-flex flex-row icons d-flex align-items-center">
-                          {" "}
-                          <LikePost likes={post.likes} postId={post._id} />
-                          <small className="likeCount muted-color">
-                            {post.likes.length}
-                          </small>
                         </div>
-                        <div className="d-flex flex-row muted-color">
+                        <div className="d-flex flex-row mt-1 ellipsis">
                           {" "}
-                          {post.comments.length !== 1 && (
-                            <span>{`${post.comments.length}`} comments</span>
-                          )}
-                          {post.comments.length === 1 && <span>1 comment</span>}
-                        </div>
-                      </div>
-                      <hr />
-                      <div className="comments">
-                        {post.comments.length > 0 &&
-                          post.comments.map((comment) => {
-                            return (
-                              <div
-                                className="d-flex flex-row mb-2"
-                                key={comment._id}
+                          <small className="mr-2">
+                            {Math.floor(
+                              (new Date() - new Date(post.dateCreated)) /
+                                (1000 * 3600 * 24)
+                            )}{" "}
+                            days ago
+                          </small>{" "}
+                          {userId === post.user._id && (
+                            <div>
+                              {/* <i className="fa fa-ellipsis-h"></i> */}
+                              <DropdownButton
+                                id="dropdown-basic-button"
+                                variant="default"
+                                size="sm"
+                                title={
+                                  <span>
+                                    <i className="fa fa-ellipsis-h"></i>
+                                  </span>
+                                }
                               >
-                                <img
-                                  src={comment.user.profilePicture}
-                                  width="40"
-                                  className="rounded-image"
-                                  alt={comment.user.userName}
-                                  onClick={() =>
-                                    navigate(`/user/${post.user._id}`)
-                                  }
-                                  style={{ cursor: "pointer" }}
-                                />
-                                <div className="d-flex flex-column ml-2">
-                                  {" "}
-                                  <span
-                                    className="name"
+                                <Dropdown.Item>Update</Dropdown.Item>
+                                <DeletePost postId={post._id} />
+                              </DropdownButton>
+                            </div>
+                          )}
+                        </div>
+                      </div>{" "}
+                      {post.image && (
+                        <img
+                          src={post.image}
+                          className="img-fluid"
+                          alt={post.text}
+                        ></img>
+                      )}
+                      <div className="p-2">
+                        <p className="text-justify">{post.text}</p>
+                        <hr />
+                        <div className="d-flex justify-content-between align-items-center">
+                          <div className="d-flex flex-row icons d-flex align-items-center">
+                            {" "}
+                            <LikePost likes={post.likes} postId={post._id} />
+                            <small className="likeCount muted-color">
+                              {post.likes.length}
+                            </small>
+                          </div>
+                          <div className="d-flex flex-row muted-color">
+                            {" "}
+                            {post.comments.length !== 1 && (
+                              <span>{`${post.comments.length}`} comments</span>
+                            )}
+                            {post.comments.length === 1 && (
+                              <span>1 comment</span>
+                            )}
+                          </div>
+                        </div>
+                        <hr />
+                        <div className="comments">
+                          {post.comments.length > 0 &&
+                            post.comments.map((comment) => {
+                              return (
+                                <div
+                                  className="d-flex flex-row mb-2"
+                                  key={comment._id}
+                                >
+                                  <img
+                                    src={comment.user.profilePicture}
+                                    width="40"
+                                    className="rounded-image"
+                                    alt={comment.user.userName}
                                     onClick={() =>
-                                      navigate(`/user/${comment.user._id}`)
+                                      navigate(`/user/${post.user._id}`)
                                     }
                                     style={{ cursor: "pointer" }}
-                                  >
-                                    {comment.user.name}
-                                  </span>{" "}
-                                  <small className="comment-text">
-                                    {comment.comment}
-                                  </small>
-                                  <div className="d-flex flex-row align-items-center status">
+                                  />
+                                  <div className="d-flex flex-column ml-2">
                                     {" "}
+                                    <span
+                                      className="name"
+                                      onClick={() =>
+                                        navigate(`/user/${comment.user._id}`)
+                                      }
+                                      style={{ cursor: "pointer" }}
+                                    >
+                                      {comment.user.name}
+                                    </span>{" "}
+                                    <small className="comment-text">
+                                      {comment.comment}
+                                    </small>
+                                    <div className="d-flex flex-row align-items-center status">
+                                      {" "}
+                                    </div>
                                   </div>
+                                  {userId === comment.user._id && (
+                                    <DeleteComment commId={comment._id} />
+                                  )}
                                 </div>
-                                {userId === comment.user._id && (
-                                  <DeleteComment commId={comment._id} />
-                                )}
-                              </div>
-                            );
-                          })}
-                        <AddComment postId={post._id} />
+                              );
+                            })}
+                          <AddComment postId={post._id} />
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
       </PostDiv>
     );
   } else if (loading) {
-    return  <PostDiv><div className="displayContainer">Loading...</div> </PostDiv>;
+    return (
+      <PostDiv>
+        <div className="displayContainer">Loading...</div>{" "}
+      </PostDiv>
+    );
   } else if (error) {
-    return  <PostDiv><div className="displayContainer">{error.message}</div> </PostDiv>;
+    return (
+      <PostDiv>
+        <div className="displayContainer">{error.message}</div>{" "}
+      </PostDiv>
+    );
   }
 };
 
 export default Posts;
 
-
 const PostDiv = styled.div`
-
-flex:0.7;
-flex-grow:1;
-overflow-y: scroll;
-margin-top: 4%;
-float: left;
-
-`
+  flex: 0.7;
+  flex-grow: 1;
+  overflow-y: scroll;
+  margin-top: 4%;
+  float: left;
+`;
