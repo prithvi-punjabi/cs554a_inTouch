e95264ad8343c4fe2f7467f9184fe2594bbe3d2e
nmghelani <nevilghelani99@gmail.com>
2022-05-09 12:53:58 -0400

    Updated s3 config

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,6 +1,6 @@
 import AWS from "aws-sdk";
 import * as toxicity from "@tensorflow-models/toxicity";
-const S3_BUCKET = "cs-546-in-touch";
+const S3_BUCKET = "intouch";
 AWS.config.update({
   accessKeyId: process.env.REACT_APP_AWS_ID,
   secretAccessKey: process.env.REACT_APP_AWS_SECRET,
--- client/webpack.config.js
+++
@@ -1,6 +0,0 @@
-// webpack.config.js
-const Dotenv = require("dotenv-webpack");
-
-module.exports = {
-  plugins: [new Dotenv()],
-};
